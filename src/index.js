34dad91b01904e48d35169495c921be8ddc11b49
Erik Bonn <erikbonn@workfront.com>
2020-04-03 16:20:04 -0600

    add es translation locale and dynamically load the correct version based on browser lang settings

--- src/index.js
+++ src/index.js
@@ -5,9 +5,32 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { IntlProvider } from "react-intl";
+// import en from "react-intl/locale-data/en";
+// import es from "react-intl/locale-data/es";
+
+import localeData from "./../build/locales/data.json";
+
+// addLocaleData([...en, ...es]); // no longer needed
+
+// Define user's language. Different browsers have the user locale defined
+// on different fields on the `navigator` object, so we make sure to account
+// for these different by checking all of them
+const language =
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage;
+
+// Split locales with a region code
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+// Try full locale, try locale without region code, fallback to 'en'
+const messages =
+  localeData[languageWithoutRegionCode] ||
+  localeData[language] ||
+  localeData.en;
 
 ReactDOM.render(
-  <IntlProvider>
+  <IntlProvider locale={language} messages={messages}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
