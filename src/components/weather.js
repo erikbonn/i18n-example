365adaf224cb1ee3f3309c92c26f5ba6bd9639c0
Erik Bonn <erikbonn@workfront.com>
2020-04-03 16:39:11 -0600

    Add photo and change text

--- src/components/weather.js
+++ src/components/weather.js
@@ -3,12 +3,18 @@ import { FormattedMessage } from "react-intl";
 
 function Weather() {
   return (
-    <p>
-      <FormattedMessage
-        id="Weather.message"
-        defaultMessage="Because it is sunny!"
-      />
-    </p>
+    <>
+      <p>
+        <FormattedMessage
+          id="Weather.message"
+          defaultMessage="Because it is sunny ya goof!"
+        />
+      </p>
+      <img
+        src="https://api.creativecommons.engineering/t/600/https://live.staticflickr.com/2660/3689874057_b972130eca_m.jpg"
+        alt="sunny day"
+      ></img>
+    </>
   );
 }
 
